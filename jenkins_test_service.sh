6c3a20f277a57425c78dab723b9501172a286e2f
lovebootcaptain <loveboatcaptain77@googlemail.com>
2017-02-17 00:09:00 +0100

    test commit

--- jenkins_test_service.sh
+++ jenkins_test_service.sh
@@ -41,6 +41,8 @@ DAEMON_NAME=jenkins_test
 # Add any command line options for your daemon here
 DAEMON_OPTS=""
 
+
+
 # This next line determines what user the script runs as.
 # Root generally not recommended but necessary if you are using the Raspberry Pi GPIO from Python.
 DAEMON_USER=root
