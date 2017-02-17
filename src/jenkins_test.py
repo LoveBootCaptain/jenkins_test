32a801bbea53dbae53651f1d16e3abcf8fd145d1
lovebootcaptain <loveboatcaptain77@googlemail.com>
2017-02-17 01:02:47 +0100

    test commit

--- src/jenkins_test.py
+++ src/jenkins_test.py
@@ -5,23 +5,21 @@ import time
 
 import pygame
 
-pygame.init()
-
-pygame.mouse.set_visible(False)
+FILE = __file__
 
 PATH = sys.path[1] + '/src'
-
-file = __file__
-
-print('file: {}'.format(file))
-
-print(PATH)
 
 FOLDER_PATH = '/FOLDER_1/'
 
 IMAGE_PATH = PATH + FOLDER_PATH + 'jenkins.png'
 
-print(IMAGE_PATH)
+print('file:    {}'.format(FILE))
+print('path:    {}'.format(PATH))
+print('image:   {}'.format(IMAGE_PATH))
+
+pygame.init()
+
+pygame.mouse.set_visible(False)
 
 DISPLAY_WIDTH = 266
 DISPLAY_HEIGHT = 266
@@ -38,17 +36,21 @@ BLUE = (52, 152, 219)
 YELLOW = (241, 196, 15)
 ORANGE = (238, 153, 18)
 
+BACKGROUND_COLOR = YELLOW
+FONT_COLOR = BLACK
+
+TEST_STRING = 'jenkins'
+
 TFT = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 
-image = pygame.image.load(IMAGE_PATH)
-font = pygame.font.SysFont('arial', 18)
-
-background = pygame.draw.rect(TFT, BLUE, (0, 0, 266, 266))
+IMAGE = pygame.image.load(IMAGE_PATH)
+FONT = pygame.font.SysFont('arial', 18)
 
 
 def draw_to_tft():
-    TFT.blit(image, (5, 5))
-    TFT.blit(font.render('jenkins', True, WHITE), (200, 240))
+    pygame.draw.rect(TFT, BACKGROUND_COLOR, (0, 0, 266, 266))
+    TFT.blit(IMAGE, (5, 5))
+    TFT.blit(FONT.render(TEST_STRING, True, FONT_COLOR), (200, 240))
 
     pygame.display.update()
 
